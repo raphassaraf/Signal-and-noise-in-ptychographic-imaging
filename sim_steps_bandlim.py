1f9727177c90044200ac2f324647e647b366bc66
Assaraf Raphael <ext-assaraf_r@ra-l-004.psi.ch>
2024-12-09 10:17:33 +0100

    Added sim_steps_bandlim simulation script

---
+++ sim_steps_bandlim.py
@@ -0,0 +1,73 @@
+import argparse
+import itertools
+import numpy as np
+import torch as t
+
+from tools.rec_scripts import reconstruct_flu
+from tools.utils import *
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        prog='steps_bandlim_reconstruction',
+        description=(
+            'Run a reconstruction sweep over scanning step size going from 1 to 30.'
+            'Use a 5 pixelwide band-limited random probe.'
+            'For each step size, reconstructions are performed over different fluences.'
+        )
+    )
+    parser.add_argument(
+        'steps_size',
+        type=int,
+        help=(
+            'The scanning steps size.'
+        )
+    )
+    parser.add_argument(
+        'n_fluences',
+        type=int,
+        help='The number of fluences between 1e-1 and 1e6 to simulate.'
+    )
+    args = parser.parse_args()
+
+    steps_size = args.steps_size
+    print(f'Steps size: {steps_size}')
+
+    n_flu = args.n_fluences
+    flu_i, flu_f = -1, 6
+    fluences = np.logspace(flu_i, flu_f, n_flu)
+    
+    rec_dict = {
+        'object_mse': None,
+        'loss_mse': None,
+        'object_mse_nll': None,
+        'loss_mse_nll': None
+    }
+    
+    ### MSE ###
+    rec_dict['object_mse'], rec_dict['loss_mse'] = reconstruct_flu(
+        fluences,
+        'MSE',
+        steps_size=steps_size,
+        multiprobe='band_lim',
+        n_probes=1
+    )
+    save_output(rec_dict, f'outputs/steps_bandlim/rec_steps_bandlim_{steps_size}.pkl')
+        
+    ### MSE --> NLL ###
+    rec_dict['object_mse_nll'], rec_dict['loss_mse_nll'] = reconstruct_flu(
+        fluences,
+        'MSE',
+        'PoissonNLL',
+        steps_size=steps_size,
+        multiprobe='band_lim',
+        n_probes=1
+    )
+            
+    save_output(rec_dict, f'outputs/steps_bandlim/rec_steps_bandlim_{steps_size}.pkl')
+
+
+if __name__ == '__main__':
+
+    main()
